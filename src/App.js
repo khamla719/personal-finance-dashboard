94ab1ef49536084d5b47e082fa3cd7a982e5849c
khamla PC <khamla719@gmail.com>
2024-08-04 00:18:48 -0400

    added pages to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashboardLayout from './layouts/DashboardLayout';
+import Dashboard from './pages/Dashboard';
+import Spending from './pages/Spending';
+import Savings from './pages/Savings';
+import Investments from './pages/Investments';
+import Settings from './pages/Settings';
+import './App.css'; // Make sure you have this file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <DashboardLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/spending" element={<Spending />} />
+          <Route path="/savings" element={<Savings />} />
+          <Route path="/investments" element={<Investments />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </DashboardLayout>
+    </Router>
   );
 }
 
---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Adjust your account settings here.</p>
+    </div>
+  );
+}
+
+export default Settings;
